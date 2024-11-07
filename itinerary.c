d8189ba27b19ee5cb2ec3b754a4060ec49d5e6fb
MethodiosZ <methzaxa@gmail.com>
2024-11-07 20:04:37 +0200

    agamisou

--- itinerary.c
+++ itinerary.c
@@ -25,7 +25,11 @@ typedef struct Student{
 
 student *StopA = NULL, *Bus = NULL, *University = NULL, *StopB = NULL; //Initialize lists for StopA, Bus, University, StopB
 pthread_mutex_t mutex;
+pthread_barrier_t barrier;
+sem_t arrival;
 int count=0;
+int BusLOcation = 1; //1 for StopA / 2 for StopB
+int MaxDepInBus[] = {0,0,0,0};
 
 void printList(student* list){
   while(list!=NULL){
@@ -49,7 +53,8 @@ void printLists(){
 void* Studying(void* vargp){
   int studytime = rand() % (15-5+1) + 5;
   int dep = rand() % (3+1);
-  pthread_mutex_lock(&mutex);
+  int nextjumps=0;
+  pthread_mutex_lock(&mutex); //lock is needed to keep count stable
   printf("\nStudent %d (%s) created.\n",1+count++,Departments[dep]);
   student *new = (student*)malloc(sizeof(student));
   new->uid = count;
@@ -65,17 +70,58 @@ void* Studying(void* vargp){
   }
   printLists();
   pthread_mutex_unlock(&mutex);
-  //printf("\nEntered University\n");
-  //sleep(studytime);
-  //printf("Leaving after studying for %d seconds\n",studytime);
+  pthread_barrier_wait(&barrier);
+  pthread_mutex_lock(&mutex);
+  if(Bus==NULL){
+    printf("\nStudent %d (%s) boarded to the bus.\n",StopA->uid,Departments[StopA->department]);
+    Bus = StopA;
+    StopA = StopA->next;
+    Bus->next = NULL;
+    printLists();
+  } else if(MaxDepInBus[StopA->department]>=N/4) {
+      printf("\nStudent %d (%s) cannot enter the bus\n",StopA->uid,Departments[StopA->department]);
+      printLists();
+      nextjumps++;
+  } else {
+    printf("\nStudent %d (%s) boarded to the bus.\n",StopA->uid,Departments[StopA->department]);
+    MaxDepInBus[StopA->department]++;
+    student* temp = Bus;
+    while(temp->next!=NULL){
+      temp = temp->next;
+    }
+    student* jump = StopA;
+    for(int i=0;i<nextjumps;i++){
+      jump=jump->next;
+    }
+    nextjumps=0;
+    temp->next = jump;
+    jump = jump->next;
+    temp->next->next = NULL;
+    printLists();
+  }
+  pthread_mutex_unlock(&mutex);
+  sem_wait(&arrival);
+  printf("\nEntered University\n");
+  sem_post(&arrival);
+  sleep(studytime);
+  printf("Leaving after studying for %d seconds\n",studytime);
   return NULL;
 }
 
 void* BusMove(void* vargp){
-  sleep(3);
-  printf("\nBus is on the way to %s\n",vargp);
-  sleep(T);
-  printf("\nBus arrived at %s\n",vargp);
+  pthread_barrier_wait(&barrier); //Wait for initialization of students
+  while(StopA!=NULL || StopB!=NULL){
+    sleep(3); //Bus waits 3 seconds to stop A
+    puts("\nBus is on the way to University");
+    sleep(T); //Bus is moving
+    puts("\nBus arrived at University");
+    sem_post(&arrival);
+    sleep(3); //Bus waits 3 seconds to stop B
+    puts("\nBus is on the way to stop A");
+    sleep(T); //Bus is moving
+    puts("\nBus arrived at stop A");
+  }
+  return NULL;
 }
 
 int main(){
@@ -88,7 +134,9 @@ int main(){
     return 1;
   }
   pthread_mutex_init(&mutex,NULL);
-  if(pthread_create(&bus,NULL,BusMove,"University")!=0){
+  pthread_barrier_init(&barrier,NULL,NoS+1);
+  sem_init(&arrival,0,0);
+  if(pthread_create(&bus,NULL,BusMove,NULL)!=0){
     perror("Failed to create bus thread\n");
   }
   pthread_t students[NoS]; //Declare student threads
@@ -105,25 +153,9 @@ int main(){
   if(pthread_join(bus,NULL) != 0){
     perror("Failed to join bus thread\n");
   }
-  /* while(StopA!=NULL){
-    printf("\nStudent %d (%s) boarded to the bus.\n",StopA->uid,Departments[StopA->department]);
-    if(Bus==NULL){
-      Bus = StopA;
-      StopA = StopA->next;
-      Bus->next = NULL;
-    } else if(MaxDepInBus[StopA->department]>=N/4) {
-      printf("\nStudent %d (%s) cannot enter the bus\n",StopA->uid,Departments[StopA->department]);
-    } else {
-      MaxDepInBus[StopA->department]++;
-      student* temp = Bus;
-      while(temp->next!=NULL){
-	temp = temp->next;
-      }
-      temp->next = StopA;
-      StopA = StopA->next;
-      temp->next->next = NULL;
-  }*/
   pthread_mutex_destroy(&mutex);
+  pthread_barrier_destroy(&barrier);
+  sem_destroy(&arrival);
   puts("\nAll students from stop A went to the University and came back");
   return 0;
 }
